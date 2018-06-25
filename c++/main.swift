4612b18f0b9b545c839531d7742c7771ed3b0af2
20161104574 <a20161104574@mac18.local>
2018-06-25 11:30:09 +0800

    Initial Commit

---
+++ c++/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  c++
+//
+//  Created by 20161104574 on 18/6/25.
+//  Copyright © 2018年 20161104574. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
